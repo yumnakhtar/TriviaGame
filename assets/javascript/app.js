57946517e4d5af7adc28a60e20b28e622c5ee81e
yumnaaa <yumnaakhtar@hotmail.com>
2018-05-02 21:32:41 -0700

    add skeleton for allQuestions object

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+//use objects to store questions and answers
+
+var allQuestions = {
+    'question1': {
+        question: "this is question 1",
+        options: ["1", "2", "3", "4"]
+    },
+    'question2': {
+        question: "this is question 2",
+        options: ["1", "2", "3", "4"]
+    },
+    'question3':{
+        question: "this is question 3",
+        options: ["1", "2", "3", "4"]
+    },
+    'question4':{
+        question: "this is question 4",
+        options: ["1", "2", "3", "4"]
+    },
+    'question5':{ 
+        question: "this is question 5",
+        options: ["1", "2", "3", "4"]
+    },
+    'question6':{
+        question: "this is question 6",
+        options: ["1", "2", "3", "4"]
+    },
+    'question7':{
+        question: "this is question 7",
+        options: ["1", "2", "3", "4"]
+    },
+    'question8':{
+        question: "this is question 8",
+        options: ["1", "2", "3", "4"]
+    },
+    'question9':{
+        question:"this is question 9",
+        options: ["1", "2", "3", "4"]
+    },
+    'question10':{
+        question:"this is question 10",
+        options: ["1", "2", "3", "4"]
+    }
+}
+
