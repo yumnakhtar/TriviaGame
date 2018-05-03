b4f54dcf30a2cd6d9008ef7f0d0e4ea93bb5966b
yumnaaa <yumnaakhtar@hotmail.com>
2018-05-02 21:43:47 -0700

    add pseudo for js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,3 +43,20 @@ var allQuestions = {
     }
 }
 
+//timer running, have x seconds
+    //give alert when 10 seconds remaining
+    //give alert when time out
+
+//select an option 
+    //have option to change mind
+    //button change color when chosen
+    //change color when actively clicking
+    //change color when hovering
+//decrement "you have x questions remaning" after selecting an answer for each question
+//submit button and timer out do same thing (use ||)
+    //when complete
+    //compare chosen answer to correct answer
+    //increment correct if correct
+    //increment wrong if wrong
+    //calculate score
+    //send message with score
