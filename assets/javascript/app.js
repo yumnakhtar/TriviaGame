1719bcd7c61335b9d10812f5c7f7400626ad17c2
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-05-29 21:56:37 -0700

    display options -temporary fix-

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,77 +1,64 @@
-//need to add windows.onload = function() {
-// 
-// }
-// where to add it?
-
 var allQuestions = [
     {
         question: "Who said this? “I saw wedding crashers accidentally. I bought a ticket for “Grizzly Man” and went into the wrong theatre. After an hour, I figured I was in the wrong theatre, but I kept waiting. Cuz that’s the thing about bear attacks… they come when you least expect it.”",
         options: ["Dwight", "Michael", "Creed", "Meredith"],
-        correct: 0
+        correct: "a"
     },
     {
         question: "What item of clothing does Michael insist on having dry cleaned?",
         options: ["Socks", "Ties", "Jeans", "Jacket"],
-        correct: 2
+        correct: "c"
     },
     {
         question: "Where does Ryan work after he gets fired from corporate?",
         options: ["Wendy's", "Mall Kiosk", "Bowling Alley", "Self-employed"],
-        correct: 2
+        correct: "c"
     },
     {
         question: "What is the fake persona that Jim uses to author the book that Dwight uses to plan Andy's garden party in the season 8 episode, 'Garden Party'?",
         options: ["James Trickington", "Jimothy Halpertini", "John Prankington", "Jimmt Foolerino"],
-        correct: 0
+        correct: "a"
     },
     {
         question: "In the episode, 'Diwali', who attempts to kiss Pam?",
         options: ["Jim", "Michael", "Roy", "Toby"],
-        correct: 1
+        correct: "b"
     },
     {
         question: "When did Ryan and Kelly hook up for the first time?",
         options: ["New Years Eve", "Halloween", "Christmas", "February 13"],
-        correct: 3
+        correct: "d"
     },
     {
         question: "What did Ryan leave in the toaster oven that caused the fire in the episode, 'The Fire'?",
         options: ["Cheese pita", "Bagel", "Poptart", "Pita Bread"],
-        correct: 0
+        correct: "a"
     },
     {
         question: "What nickname does Pam not come up with for Dwight's bed and breakfast?",
         options: ["The Embassy Beets", "The Beets Motel", "The Radish Inn", "The Borscht Motel"],
-        correct: 3
+        correct: "d"
     },
     {
-        question: "What does Jim NOT do to pay Dwight back for saving him from Roy",
+        question: "What does Jim NOT do to pay Dwight back for saving him from Roy?",
         options: ["Get him a glass for his bobble", "Buy him beet seeds", "keep quiet about Dwight and Angela's affair", "Make him a certificate for bravery"],
-        correct: 1
+        correct: "b"
     },
     {
         question: "How soon after Jim and Pam started dating did Jim buy an engagement ring?",
         options: ["One Month", "One Week", "Three Weeks", "Two Days"],
-        correct: 1
+        correct: "b"
     }
 ]
 
-
+var storeQs = $("#storeQuestions");
 
 //use objects to store questions and answers
 window.onload = function () {
+    printQuestion();
+ 
 
-    for (var i = 0; i < allQuestions.length; i++) {
-        var print = "testing " + i;
-        var QUESTION = ("Question " + [i+1] + ": " + allQuestions[i].question);
-
-        var hook = $("#storeQuestions");
-        var newDiv = $("<div class='eachQuestion'>");
-        newDiv.text(QUESTION);
-        hook.append(newDiv)
-    }
-
-    $("#stop").on("click", stopwatch.stop);
+    $("#submit").on("click", stopwatch.stop);
     $("#start").on("click", stopwatch.start);
 
 }
@@ -105,11 +92,6 @@ var stopwatch = {
         $(".display").text(converted);
 //////place if statement to give a 10 second warning.
 //////if statement to change page when timer is at 00:00
-
-
-
-
-
     },
     timeConverter: function (t) {
         var minutes = Math.floor(t / 60);
@@ -128,7 +110,24 @@ var stopwatch = {
     }
 };
 
+function printQuestion(){
+    for (var i = 0; i < allQuestions.length; i++) {
+        var QUESTION = ("Question " + [i+1] + ": " + allQuestions[i].question);
+        var QuestionDiv = $("<div class='eachQuestion'>");
+        QuestionDiv.text(QUESTION);
+        storeQs.append(QuestionDiv)
+        printOptions(i);
+    }
+}
 
+function printOptions(i) {
+    for(var j=0; j<allQuestions[i].options.length; j++){
+        var options = allQuestions[i].options[j];
+        var optionsDiv = $("<div class='eachOption'>");
+        optionsDiv.text(options);
+        storeQs.append(optionsDiv);
+    }
+}
 
 //use for loop to print all questions on!!
 // console.log(allQuestions.question1.question);
